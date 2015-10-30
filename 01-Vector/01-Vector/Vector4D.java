3449fe8090882cc6979ba5e9211387a2cc2742fa
Mardiana Safutri <safutrimardiana@gmail.com>
2015-10-30 17:57:40 +0700

    no message

---
+++ 01-Vector/01-Vector/Vector4D.java
@@ -0,0 +1,29 @@
+
+public class Vector3D
+{
+    public double x, y, z;
+   
+
+    /**
+     * Constructor for objects of class Vektor3D
+     */
+    public Vector3D(double x, double y, double z)
+    {
+        this. x=x;
+        this. y=y;
+        this. z=z;
+    }
+   public double getX () {
+       return x;
+    }
+    public double getY () {
+       return y;
+    }
+    public double getZ () {
+       return z;
+    }
+    
+    
+      
+     
+}
