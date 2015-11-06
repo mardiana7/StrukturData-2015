1aabce29a80e8e729c69ba22b0086004433733b0
Mardiana Safutri <safutrimardiana@gmail.com>
2015-11-06 14:48:35 +0700

    no message

---
+++ 04-Profil-Test-Awal/04-Profil-Test-Awal/Profil.java
@@ -0,0 +1,31 @@
+package Komdat;
+
+
+ 
+public class Profil
+{
+    
+    private String nama = new String ("Mardiana Safutri");
+    private String nim = new String("1408107010052");
+
+     
+    public Profil(){
+    }
+ 
+    public Profil (String nama, String nim)
+    {
+        this.nama = nama;
+        this.nim = nim;
+    }
+     
+    public String getNama()
+    {
+  
+       return nama; 
+    }
+    
+    public String getNim()
+    {
+        return nim;
+    }
+}
